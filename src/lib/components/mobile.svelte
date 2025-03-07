4f870aee2150861370823d3bc2930d117a68c4ac
uchkunovdaniel <du98130933@edu.mon.bg>
2025-03-07 10:29:43 +0200

    Fix

--- src/lib/components/mobile.svelte
+++ src/lib/components/mobile.svelte
@@ -2,6 +2,8 @@
 	Изтегли мобилното приложение!
 </h1>
 
+<a href="https://github.com/uchkunovdaniel/Chudoznaiko/releases/download/v1/default.apk">Изтегли</a>
+
 <style>
 	h1 {
 			text-align: center;
@@ -10,5 +12,25 @@
 			font-family: "transforma", sans-serif;
 			font-weight: 900;
 			user-select: none;
+	}
+	a{
+				background: var(--main);
+        width: 20vw;
+        height: 14vw;
+        border: none;
+        border-radius: 0.5rem;
+        font-family: "transforma", sans-serif;
+        font-size: 10vw;
+        font-weight: 900;
+        color: white;
+        outline: none;
+        cursor: pointer;
+        transition: ease 0.25s;
+        box-shadow: rgba(255, 255, 255, 0.38) 0 0 5px;
+        user-select: none;
+				text-align: center;
+				position: relative;
+				left: 24vw;
+				padding: 1vw;
 	}
 </style>
